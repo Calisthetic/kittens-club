c26f0f7a671830e95dba05a77c6471805bf35be4
Calisthetic <vovakalash04@mail.ru>
2024-06-17 15:10:03 +0500

    swipe events

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@
 
 import Sidebar from "./sidebar/sidebar";
 import SidebarButton from "./sidebar/sidebar-button";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function Header({username}:{username:string|null}) {
@@ -21,6 +21,51 @@ export default function Header({username}:{username:string|null}) {
       window.removeEventListener("resize", ChangeSideBar)
     }
   }, [])
+
+
+
+
+  useEffect(()=>{
+    document.addEventListener("touchstart", startTouch, false);
+    document.addEventListener("touchend", endTouch, false);
+    document.addEventListener("touchmove", moveTouch, false);
+
+    return () => {
+      document.addEventListener("touchstart", startTouch, false);
+      document.addEventListener("touchend", endTouch, false);
+      document.addEventListener("touchmove", moveTouch, false);
+    }
+  }, [isSideBarOpen])
+
+  // Swipe Up / Down / Left / Right
+  const initialX = useRef<number|null>(null);
+
+  function startTouch(e:any) {
+    initialX.current = e.touches[0].clientX;
+  };
+
+  function endTouch(e:any) {
+    initialX.current = null
+  };
+
+  function moveTouch(e:any) {
+    if (initialX.current === null) {
+      return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var diffX = initialX.current - currentX;
+
+    if (diffX > 100) {
+      // swiped left
+      if (isSideBarOpen)
+        setIsSideBarOpen(false)
+    } else if (diffX < -100) {
+      // swiped right
+      if (!isSideBarOpen)
+        setIsSideBarOpen(true)
+    }
+  };
 
   return (
     <>
